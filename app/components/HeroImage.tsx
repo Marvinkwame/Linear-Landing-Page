1a7752c987f270ceafb9c3f60ffc1f7a679ec08b
Marvinkwame <marvinkudjo@gmail.com>
2024-04-05 00:05:03 +0000

    SVG Lines animation

--- app/components/HeroImage.tsx
+++ app/components/HeroImage.tsx
@@ -11,6 +11,21 @@ const HeroImage = () => {
         <div ref={ref} className="[perspective:2000px] mt-[12.8rem]">
             <div className={classNames('relative bg-hero-gradient border border-transparent bg-white bg-opacity-[0.01] rounded-lg ', inView ? "animate-image-rotate" : "[transform:rotateX(25deg)]",
                 "before:w-full before:h-full before:top-0 before:left-0 before:absolute before:bg-hero-glow before:[filter:blur(120px)] before:opacity-0", inView && "before:animate-image-glow")}>
+                <svg
+                className={classNames(
+                    "absolute left-0 top-0 w-full h-full",
+                    '[&_path]:stroke-white [&_path]:[stroke-opacity:0.2] [&_path]:[stroke-dasharray:1] [&_path]:[stroke-dashoffset:1]',
+                inView && "[&_path]:animate-sketch-lines" )}
+                    width="100%"
+                    viewBox='0 0 1499 778'
+                    fill="none"
+                >
+                    <path pathLength="1" d="M1500 72L220 72"></path>
+                    <path pathLength="1" d="M1500 128L220 128"></path>
+                    <path pathLength="1" d="M1500 189L220 189"></path>
+                    <path pathLength="1" d="M220 777L220 1"></path>
+                    <path pathLength="1" d="M538 777L538 128"></path>
+                </svg>
                 <img
                     src="/img/hero.webp"
                     alt="Hero Image"
